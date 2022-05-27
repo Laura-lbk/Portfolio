acf1430c71fb9b0b7f5e90054333c1df3c421e25
Laura <lbk.laura@gmail.com>
2022-05-27 21:48:41 +0200

    Test pour GithubPages

--- src/app/Pages/homepage/homepage.component.ts
+++ src/app/Pages/homepage/homepage.component.ts
@@ -14,7 +14,7 @@ export class HomepageComponent implements OnInit {
   text1: string = '';
   text2: string = '';
   text3: string = '';
-  age: number;
+  age: number = 0;
   TITLE1: string = '';
   TITLE2: string = '';
   TITLE3: string = '';
@@ -30,28 +30,31 @@ export class HomepageComponent implements OnInit {
 
 
   constructor(public translate: TranslateService) {
-      this.translate.get('HOMEPAGE').subscribe((translated: string) => {
-        let parsed = JSON.parse(JSON.stringify(translated));
-        this.titre1 = parsed.TITRE1;
-        this.text1 = parsed.TEXT1;
-        this.text2 = parsed.TEXT2;
-        this.text3 = parsed.TEXT3;
-        this.titreCarte2 = parsed.TITRECARTE2;
-        this.TITLE1 = parsed.CARTE2.TITLE1;
-        this.TITLE2 = parsed.CARTE2.TITLE2;
-        this.TITLE3 = parsed.CARTE2.TITLE3;
-        this.TITLE = parsed.CARTE2.LEGENDE.TITLE;
-        this.BASES = parsed.CARTE2.LEGENDE.BASES;
-        this.INTER = parsed.CARTE2.LEGENDE.INTER;
-        this.V = parsed.CARTE2.GESTION.V;
-        this.AGILE = parsed.CARTE2.GESTION.AGILE;
-        this.MAETIC = parsed.CARTE2.GESTION.MAETIC;
-        this.THEORIE = parsed.CARTE2.GESTION.THEORIE;
-        this.STRATEGIE = parsed.CARTE2.GESTION.STRATEGIE;
+      this.getTranslation();
+  }
 
-      });
-      this.age = this.calculateAge('04/28/1999');
-      console.log(this.age);
+  getTranslation(){
+    this.translate.get('HOMEPAGE').subscribe((translated: string) => {
+      let parsed = JSON.parse(JSON.stringify(translated));
+      this.titre1 = parsed.TITRE1;
+      this.text1 = parsed.TEXT1;
+      this.text2 = parsed.TEXT2;
+      this.text3 = parsed.TEXT3;
+      this.titreCarte2 = parsed.TITRECARTE2;
+      this.TITLE1 = parsed.CARTE2.TITLE1;
+      this.TITLE2 = parsed.CARTE2.TITLE2;
+      this.TITLE3 = parsed.CARTE2.TITLE3;
+      this.TITLE = parsed.CARTE2.LEGENDE.TITLE;
+      this.BASES = parsed.CARTE2.LEGENDE.BASES;
+      this.INTER = parsed.CARTE2.LEGENDE.INTER;
+      this.V = parsed.CARTE2.GESTION.V;
+      this.AGILE = parsed.CARTE2.GESTION.AGILE;
+      this.MAETIC = parsed.CARTE2.GESTION.MAETIC;
+      this.THEORIE = parsed.CARTE2.GESTION.THEORIE;
+      this.STRATEGIE = parsed.CARTE2.GESTION.STRATEGIE;
+
+    });
+    this.age = this.calculateAge('04/28/1999');
   }
 
   ngOnInit(): void {
--- src/app/Pages/scolaire/scolaire.component.ts
+++ src/app/Pages/scolaire/scolaire.component.ts
@@ -1,13 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+import {TranslateService} from "@ngx-translate/core";
 
 @Component({
   selector: 'app-scolaire',
   templateUrl: './scolaire.component.html',
-  styleUrls: ['./scolaire.component.css']
+  styleUrls: ['./scolaire.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class ScolaireComponent implements OnInit {
 
-  constructor() { }
+  MASTER: string = '';
+  LICENCE: string = '';
+  DUT: string = '';
+  BAC: string = '';
+
+  constructor(public translate: TranslateService) {
+    this.getTranslation();
+  }
+
+  getTranslation(){
+    this.translate.get('SCOLARITE').subscribe((translated: string) => {
+      let parsed = JSON.parse(JSON.stringify(translated));
+      this.MASTER = parsed.MASTER;
+      this.LICENCE = parsed.LICENCE;
+      this.DUT = parsed.DUT;
+      this.BAC = parsed.BAC;
+    });
+
+  }
 
   ngOnInit(): void {
   }
